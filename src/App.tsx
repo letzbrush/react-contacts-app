9f160e307d36c58273aeba7f670e2344d7a811c4
Matt <matthias.meier@move-digital.ch>
2021-12-03 16:37:34 +0100

    feat: basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Layout } from 'antd'
+
+import styles from './App.module.less'
+
+const { Header, Content, Footer } = Layout
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className={styles.layout}>
+    <Header>Header</Header>
+    <Content>
+      Content
+    </Content>
+    <Footer>Footer</Footer>
+    </Layout>
   )
 }
 
