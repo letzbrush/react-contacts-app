fde4d0c95bd4af7d2da9d590d31b2be893fc9719
Matt <matthias.meier@move-digital.ch>
2021-12-04 12:51:53 +0100

    style: basic setup (vars & fonts)

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.less'
+import './styles/index.less'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './store/store'
