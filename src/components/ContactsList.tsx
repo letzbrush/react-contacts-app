888cc9af0afb5c992265fe0c7c12745429a0f324
Matt <matthias.meier@move-digital.ch>
2021-12-04 17:06:26 +0100

    fix(ContactsList): remove wrong PageContent wrapping

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { List, Avatar } from 'antd'
 
 import { Contact } from '../types/Contact'
-import PageContent from './Layout/PageContent'
 
 interface ContactsListProps {
   contacts: Contact[]
@@ -10,21 +9,19 @@ interface ContactsListProps {
 
 const ContactsList = ({ contacts }: ContactsListProps) => {
   return (
-    <PageContent>
-      <List
-        itemLayout="horizontal"
-        dataSource={contacts}
-        renderItem={contact => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={contact.avatarUrl} size='large'/>}
-              title={contact.name}
-              description={contact.company.catchPhrase}
-            />
-          </List.Item>
-        )}
-      />
-    </PageContent>
+    <List
+      itemLayout="horizontal"
+      dataSource={contacts}
+      renderItem={contact => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src={contact.avatarUrl} size='large'/>}
+            title={contact.name}
+            description={contact.company.catchPhrase}
+          />
+        </List.Item>
+      )}
+    />
   )
 }
 
